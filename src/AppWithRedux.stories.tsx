306398ed3295b27ec322165636d2591be304ae70
vovitolog <vovitolog@gmail.com>
2023-02-28 18:39:40 +0300

    feat: add ReduxStoreProviderDecorator

--- src/AppWithRedux.stories.tsx
+++ src/AppWithRedux.stories.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 import AppWithRedux from "./AppWithRedux";
-import {Provider} from "react-redux";
-import {store} from "./state/store";
+import {ReduxStoreProviderDecorator} from "./stories/ReduxStoreProviderDecorator";
 
 export default {
     title: 'AppWithRedux component',
-    component: AppWithRedux
+    component: AppWithRedux,
+    decorators: [ReduxStoreProviderDecorator]
 }
 
 export const AppWithReduxBaseExample = (props: any) => {
-    return <Provider store={store}> <AppWithRedux/> </Provider>
+    return <AppWithRedux/>
 }
 
 
---
+++ src/stories/ReduxStoreProviderDecorator.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import {Provider} from "react-redux";
+import {tasksReducer} from "../state/tasks-reducer";
+import {todolistsReducer} from "../state/todolists-reducer";
+import {v1} from 'uuid';
+import {AppRootStateType} from "../state/store";
+import {combineReducers, createStore} from "redux";
+
+const rooReducer = combineReducers({
+    tasks: tasksReducer,
+    todolist: todolistsReducer
+})
+
+const initialGlobalState = {
+    todolists: [
+        {id: "todolistId1", title: "What to learn", filter: "all"},
+        {id: "todolistId2", title: "What to buy", filter: "all"}
+    ],
+    tasks: {
+        ["todolistId1"]: [
+            {id: v1(), title: "JS", isDone: true },
+            {id: v1(), title: "HTML", isDone: true },
+        ],
+        ["todolistId2"]: [
+            {id: v1(), title: "Milk", isDone: true },
+            {id: v1(), title: "Book", isDone: true },
+        ],
+    }
+}
+
+export const storyBookStore = createStore(rooReducer, initialGlobalState as AppRootStateType);
+
+export const ReduxStoreProviderDecorator = (storyFn: any) => {
+    return <Provider store={storyBookStore}>{storyFn()}</Provider>
+}
