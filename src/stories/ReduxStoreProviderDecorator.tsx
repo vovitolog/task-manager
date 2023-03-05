89dc702172383f76fb2156d879b54d04903bc4d1
vovitolog <vovitolog@gmail.com>
2023-03-05 13:28:14 +0300

    fix: fix ReduxStoreProviderDecorator syntax and imports

--- src/stories/ReduxStoreProviderDecorator.tsx
+++ src/stories/ReduxStoreProviderDecorator.tsx
@@ -5,30 +5,43 @@ import {todolistsReducer} from "../state/todolists-reducer";
 import {v1} from 'uuid';
 import {AppRootStateType} from "../state/store";
 import {combineReducers, createStore} from "redux";
+import {TaskPriorities, TaskStatuses} from "../api/todolists-api";
 
 const rootReducer = combineReducers({
     tasks: tasksReducer,
     todolists: todolistsReducer
 })
 
-const initialGlobalState = {
+const initialGlobalState: AppRootStateType = {
     todolists: [
-        {id: "todolistId1", title: "What to learn", filter: "all"},
-        {id: "todolistId2", title: "What to buy", filter: "all"}
+        {id: "todolistId1", title: "What to learn", filter: "all", addedDate: '', order: 0},
+        {id: "todolistId2", title: "What to buy", filter: "all", addedDate: '', order: 0}
     ],
     tasks: {
         ["todolistId1"]: [
-            {id: v1(), title: "JS", isDone: true },
-            {id: v1(), title: "HTML", isDone: true },
+            {
+                id: v1(), title: "JS", status: TaskStatuses.New, todolistId: "todolistId1",
+                description: '', startDate: '', deadLine: '', addedDate: '', priority: TaskPriorities.Low, order: 0
+            },
+            {
+                id: v1(), title: "HTML", status: TaskStatuses.New, todolistId: "todolistId1",
+                description: '', startDate: '', deadLine: '', addedDate: '', priority: TaskPriorities.Low, order: 0
+            },
         ],
         ["todolistId2"]: [
-            {id: v1(), title: "Milk", isDone: true },
-            {id: v1(), title: "Book", isDone: true },
+            {
+                id: v1(), title: "Milk", status: TaskStatuses.New, todolistId: "todolistId2",
+                description: '', startDate: '', deadLine: '', addedDate: '', priority: TaskPriorities.Low, order: 0
+            },
+            {
+                id: v1(), title: "Book", status: TaskStatuses.New, todolistId: "todolistId2",
+                description: '', startDate: '', deadLine: '', addedDate: '', priority: TaskPriorities.Low, order: 0
+            },
         ],
     }
 }
 
-export const storyBookStore = createStore(rootReducer, initialGlobalState as AppRootStateType);
+export const storyBookStore = createStore(rootReducer, initialGlobalState);
 
 export const ReduxStoreProviderDecorator = (storyFn: any) => {
     return <Provider store={storyBookStore}>{storyFn()}</Provider>
