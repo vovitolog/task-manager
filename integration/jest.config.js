58e734d95cd7c7a75073218c126036d227be525c
vovitolog <vovitolog@gmail.com>
2023-03-28 18:32:26 +0300

    fix: fix jest axios error

--- integration/jest.config.js
+++ integration/jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
+   transformIgnorePatterns: ['/node_modules/(?!(axios)/)'],
    preset: 'jest-puppeteer',
    testRegex: './*\\.test\\.js$',
-   setupFilesAfterEnv: ['./setupTests.js']
+   setupFilesAfterEnv: ['./setupTests.js'],a
 };
